f3b6e18edc1edbf90e3385a3f8d3855d0c067b74
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-06-27 23:32:14 -0700

    Rework on Header And Hero components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head"
 import Link from "next/link"
 import Image from "next/image"
-import Logo from '../public/Img/Tadesse.jpg'
+import Logo from '../public/Img/Tadesse.png'
 const Header = () => {
   return (
-<header className=" bg-black font-semibold  px-12 py-6 text-gray-200">
+<header className=" bg-black font-semibold  pl-32 py-5 text-gray-300  ">
    <div className="flex justify-between items-center">
       <div className="flex items-center space-x-4">
-    <span className="text-xl">Tadesse G. Wakjira</span>
+    <Image src={Logo}   className="rounded-full" width={50} height={50}/>
+    <span>Tadesse G. Wakjira</span>
       </div>
       <div className="flex justify-between">
         <nav className="flex list-none space-x-10  hidden md:inline-flex text-black-200">
@@ -18,24 +19,29 @@ const Header = () => {
             </li>
             <li>
                 <Link href="">
-                    <a> BLOG</a>
+                    <a> PUBLICATIONS <span></span></a>
                 </Link>
             </li>
             <li>
                 <Link href="">
-                    <a> PORTFOILO</a>
+                    <a> WHAT'S NEW</a>
                 </Link>
             </li>
             <li>
                 <Link href="">
-                    <a> ABOUT</a>
+                    <a> AWARDS</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="">
+                    <a> RESEARCH</a>
                 </Link>
             </li>
            
         </nav>
   
       </div>
-      <div className="mr-32 bg-white text-black px-5 py-1  rounded-full">
+      <div className="mr-24 bg-white text-black px-5 py-1  rounded-full">
         <Link href=""  >
                     <a> CONTACT</a>
                 </Link>
